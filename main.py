00e5d1b275e6f40c05e8e8f26e36392c00d9c865
Anupama Ushetti <anupamaush69@gmail.com>
2025-05-15 10:26:38 +0530

    finished implementing a basic CLI AI assistant using LangChain and OpenAI API

--- main.py
+++ main.py
@@ -26,5 +26,15 @@ def main():
         for chunk in agent_executor.stream(
             {"messages": [HumanMessage(content=user_input)]}
         ):
+            if "agent" in chunk and "messages" in chunk ["agent"]:
+                for message in chunk ["agent"]["messages"]:
+                    print(message.content, end="")
+
+            print()
+
+if __name__ == "__main__":
+    main()         
+
+ 
 
 
